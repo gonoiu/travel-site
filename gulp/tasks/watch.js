ba67677878f634f4972097e0376e8b61e424d54e
Georgian <gonoiu@gmail.com>
2018-07-27 18:48:59 +0300

    bla

--- gulp/tasks/watch.js
+++ gulp/tasks/watch.js
@@ -19,9 +19,7 @@ gulp.task('watch', function() {
   watch('./app/assets/styles/**/*.css', function() {
     gulp.start('cssInject')
   })
-
 })
-
 
 gulp.task('cssInject', ['styles'], function() {
   return gulp.src('./app/temp/styles/styles.css')
