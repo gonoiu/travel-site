45614a292303c70af34d8be2b4dbdd6516df7c70
Georgian <gonoiu@gmail.com>
2018-07-29 18:03:37 +0300

    completed large hero block

--- gulp/tasks/styles.js
+++ gulp/tasks/styles.js
@@ -4,16 +4,14 @@ autoprefixer = require('autoprefixer'),
 cssvars = require('postcss-simple-vars'),
 nested = require('postcss-nested'),
 cssImport = require('postcss-import'),
-mixins = require('postcss-mixins')
-
+mixins = require('postcss-mixins');
 
 gulp.task('styles', function() {
   return gulp.src('./app/assets/styles/styles.css')
-  .pipe(postcss([cssImport, mixins, cssvars, autoprefixer, nested]))
-  .on('error', function(errorInfo) {
-    console.log(errorInfo.toString())
-    this.emit('end')
-  })
-  .pipe(gulp.dest('./app/temp/styles'))
-  console.log("I've changed your CSS, Matey!!! AHOY!!!")
-})
+    .pipe(postcss([cssImport, mixins, cssvars, nested, autoprefixer]))
+    .on('error', function(errorInfo) {
+      console.log(errorInfo.toString());
+      this.emit('end');
+    })
+    .pipe(gulp.dest('./app/temp/styles'));
+});
--- gulp/tasks/watch.js
+++ gulp/tasks/watch.js
@@ -1,27 +1,27 @@
 var gulp = require('gulp'),
 watch = require('gulp-watch'),
-browserSync = require('browser-sync').create()
-
+browserSync = require('browser-sync').create();
 
 gulp.task('watch', function() {
 
   browserSync.init({
-    // notify: false,
+    notify: false,
     server: {
       baseDir: "app"
     }
-  })
+  });
 
   watch('./app/index.html', function() {
-    browserSync.reload()
-  })
+    browserSync.reload();
+  });
 
   watch('./app/assets/styles/**/*.css', function() {
-    gulp.start('cssInject')
-  })
-})
+    gulp.start('cssInject');
+  });
+
+});
 
 gulp.task('cssInject', ['styles'], function() {
   return gulp.src('./app/temp/styles/styles.css')
-  .pipe(browserSync.stream())
-})
+    .pipe(browserSync.stream());
+});
